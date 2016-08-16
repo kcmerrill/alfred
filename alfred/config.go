ca2f0978f76fe9a3c293d402d39cbd85f252f364
kc merrill <kcmerrill@gmail.com>
2016-08-16 15:14:26 -0600

    Fixed an issue that required config file to be available

--- alfred/config.go
+++ alfred/config.go
@@ -1,17 +1,23 @@
 package alfred
 
 import (
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"os/user"
+
+	"gopkg.in/yaml.v2"
 )
 
+/*
+Config
+reads in the user's local yaml configuration file */
 func (a *Alfred) Config() {
 	usr, err := user.Current()
 	if err != nil {
 		return
 	}
+
+	a.config.Remote = make(map[string]string)
 
 	configFile := usr.HomeDir + "/.alfred/config.yml"
 
