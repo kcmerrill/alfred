6df096da5e7f0f9a2f934e98afcb05643579e32d
kc merrill <kcmerrill@gmail.com>
2017-03-24 10:50:13 -0600

    Reverted to using /Users/cmerrill instead of stdlib. Doesn't cross compile well. By well I mean at all

--- alfred/config.go
+++ alfred/config.go
@@ -3,7 +3,6 @@ package alfred
 import (
 	"io/ioutil"
 	"os"
-	"os/user"
 
 	"gopkg.in/yaml.v2"
 )
@@ -11,16 +10,13 @@ import (
 // Config reads in the user's configuration file stored in $HOME/.alfred/config.yml
 func (a *Alfred) Config() {
 	a.config.Remote = make(map[string]string)
-	usr, err := user.Current()
-	if err != nil {
-		return
-	}
 
-	configFile := usr.HomeDir + "/.alfred/config.yml"
+	// os.User is really terrible. Also doesn't work with cross compiling
+	configFile := os.Getenv("HOME") + "/.alfred/config.yml"
 
 	if _, statError := os.Stat(configFile); statError == nil {
 		if contents, readError := ioutil.ReadFile(configFile); readError == nil {
-			if err = yaml.Unmarshal([]byte(contents), &a.config); err != nil {
+			if err := yaml.Unmarshal([]byte(contents), &a.config); err != nil {
 				say("error", "loading config")
 				say("config", configFile)
 			}
